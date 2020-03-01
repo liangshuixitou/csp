bedb0cad9b0ac0a543f4413929106c6070eda291
code_water <1151824226@qq.com>
2020-03-01 21:28:47 +0800

    test3

--- test.cpp
+++ test.cpp
@@ -1,10 +1,10 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    cout<<"hello world";
-    cout<<"hello World!";
+    cout << "hello world";
+    cout << "hello World!";
     system("pause");
     return 0;
 }
